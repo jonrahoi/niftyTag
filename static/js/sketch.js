d32b4351b7cfa4bc23e58de6548b8ba5bff6dcba
Jon Rahoi <jon.rahoi@creditkarma.com>
2018-05-28 13:51:53 -0700

    more vehicles

--- static/js/sketch.js
+++ static/js/sketch.js
@@ -1,10 +1,18 @@
-let guy
+let guys = []
+const numOfVehicles = 10;
+const boardWidth = 800
+const boardHeight = 400;
 
 function setup() {
-  createCanvas(800, 400);
- 
-    guy = new Vehicle()
-  target = createVector(width / 2, 50);
+  createCanvas(boardWidth, boardHeight);
+    
+  for (let x=0; x< numOfVehicles; x++){
+    // pick a random x,y
+    let x = random(width)
+    let y = random(height)
+    guys.push(new Vehicle(x, y))
+  }
+    target = createVector(width / 2, 50);
 
 }
 
@@ -12,7 +20,14 @@ function draw() {
   background(0);
   fill(255, 70)
   ellipse(mouseX, mouseY, 40, 40);
-  guy.seek(createVector(mouseX, mouseY))
-  guy.update()
-  guy.display()
+
+  for (let x=0; x< numOfVehicles; x++){
+    let guy = guys[x]
+    // send a sample of what this guy can see to him
+    // then call yourdesiredvector = guy.decide(listOfPlayers, boardWidth, boardHeight, areYouIt);
+    guy.seek(createVector(mouseX, mouseY))
+    guy.update()
+    guy.display()
+  }
+  
 }
